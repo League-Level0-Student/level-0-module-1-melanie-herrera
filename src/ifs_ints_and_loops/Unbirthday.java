f96bd88242e317fb48d76b1bd4828b95ba81a5cb
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-28 13:26:26 -0800

    Unbirthday

---
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -0,0 +1,17 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String[] args) { 
+		String birthday=JOptionPane.showInputDialog("when is your birthday(mm/dd)");
+		if(birthday.equals("2/28/19")) {
+			JOptionPane.showMessageDialog(null, "happy birthday");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, vary merry unbirthday);
+		}
+		
+	}
+
+}
