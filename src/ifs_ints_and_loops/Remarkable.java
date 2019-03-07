46f6de169e7531fe09e3898942f9a53622e70687
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-03-07 13:25:25 -0800

    Remarkable Unbirthday

---
+++ src/ifs_ints_and_loops/Remarkable.java
@@ -0,0 +1,34 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Remarkable { 
+	public static void main(String[] args) { 
+String name=JOptionPane.showInputDialog(null,"whats your name"); 
+String melanie="she is so cool";
+if(name.equals("melanie")) {
+	JOptionPane.showMessageDialog(null, melanie);
+}
+String michelle="she is mean"; 
+	if(name.equals("michelle")){
+		JOptionPane.showMessageDialog(null, michelle);
+}
+	String kendra="she is small";
+	if(name.equals("kendra")) {
+		JOptionPane.showMessageDialog(null, kendra);
+	
+	}
+	String stan="he is tall";
+	if(name.equals("stan")) {
+		JOptionPane.showMessageDialog(null, stan);
+	}
+	String cesia="she is nice";
+	if(name.equals("cesia")) {
+		JOptionPane.showMessageDialog(null, cesia);
+	}
+			
+		
+}
+	
+
+}
--- src/ifs_ints_and_loops/Unbirthday.java
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -5,11 +5,11 @@ import javax.swing.JOptionPane;
 public class Unbirthday {
 	public static void main(String[] args) { 
 		String birthday=JOptionPane.showInputDialog("when is your birthday(mm/dd)");
-		if(birthday.equals("2/28/19")) {
+		if(birthday.equals("03/07")) {
 			JOptionPane.showMessageDialog(null, "happy birthday");
 		}
 		else {
-			JOptionPane.showMessageDialog(null, vary merry unbirthday);
+			JOptionPane.showMessageDialog(null, "vary merry unbirthday");
 		}
 		
 	}
