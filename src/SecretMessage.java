8bdde7827f5d71c066b1cffca9bd380293cf20cc
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-03-14 13:23:57 -0700

    SecretMessageip

---
+++ src/SecretMessage.java
@@ -0,0 +1,11 @@
+import java.util.StringJoiner;
+
+import javax.swing.JOptionPane;
+
+public class SecretMessage { public static void main(String[] args) { 
+	String password="melanie is cool";
+	String message= JOptionPane.showInputDialog("what's your secret messsage");
+	String guess= JOptionPane.showMessageDialog,("geuss the password");
+}
+
+}
