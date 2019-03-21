75c9453a96b97e43ba959bb22071fd4106319d2f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-03-21 13:28:15 -0700

    TheRiddler

--- src/SecretMessage.java
+++ src/SecretMessage.java
@@ -5,7 +5,14 @@ import javax.swing.JOptionPane;
 public class SecretMessage { public static void main(String[] args) { 
 	String password="melanie is cool";
 	String message= JOptionPane.showInputDialog("what's your secret messsage");
-	String guess= JOptionPane.showMessageDialog,("geuss the password");
+	String guess= JOptionPane.showInputDialog(null,"geuss the password");
+	if (guess.equals(password)) {
+		JOptionPane.showMessageDialog(null, message);
+	} 
+	else {
+		JOptionPane.showMessageDialog(null,"INTRUDER");
+	}
+	
 }
 
 }
---
+++ src/TheRiddler.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class TheRiddler { public static void main(String[] args) {
+	int score = 0;
+	JOptionPane.showInputDialog("");
+	
+}
+
+}
